b9825672bc72d364bef7c1602c3a0f8463dc685c
ostapkob <ostapkob.gmail.com>
2020-12-25 18:03:28 +1000

    ssl

--- server.js
+++ server.js
@@ -4,11 +4,24 @@ const cors = require("cors");
 const dbConfig = require("./app/config/db.config");
 var bcrypt = require("bcryptjs");
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync('/etc/letsencrypt/live/m1.nmtport.ru/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/m1.nmtport.ru/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/m1.nmtport.ru/chain.pem', 'utf8');
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
 const app = express();
 
 var corsOptions = {
   // origin: "http://192.168.99.107:8081"
-  // origin: "http://127.0.0.1:8081"
+  // origin: "http://127.0.0.1:8080"
 };
 
 app.use(cors(corsOptions));
@@ -18,6 +31,9 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 
 const db = require("./app/models");
 const Role = db.role;
@@ -46,11 +62,18 @@ app.get("/", (req, res) => {
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+// Starting both http & https servers
+// const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(3000, () => {
+  console.log('HTTPS Server running on port 3000 ->');
 });
+
+// set port, listen for requests
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
 
 function initial() {
   User.estimatedDocumentCount((err, count) => {
